eee2ca9b99cb4c84163cd40cd01b3df86ae8e152
Eric Walker <erictwalker18@gmail.com>
2020-08-13 10:50:18 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import subprocess
 from setuptools import setup, find_packages
 
 NAME = 'falcano'
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 AUTHOR = 'Eric Walker'
 AUTHOR_EMAIL = 'ewalker3@mmm.com'
 DESCRIPTION = 'Falcano'
