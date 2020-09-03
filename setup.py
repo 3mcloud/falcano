5959e9518f24117f76481549541a274dd1d199b9
a8xwzzz <bthompson3@mmm.com>
2020-09-03 11:13:40 -0500

    bump falcano version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import subprocess
 from setuptools import setup, find_packages
 
 NAME = 'falcano'
-VERSION = '0.0.3'
+VERSION = '0.0.4'
 AUTHOR = 'Eric Walker'
 AUTHOR_EMAIL = 'ewalker3@mmm.com'
 DESCRIPTION = 'Falcano'
