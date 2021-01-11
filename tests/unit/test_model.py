94329be770390d8393e23773768dc10c036f7362
a9phhzz <a9phhzz@mmm.com>
2021-01-11 11:47:58 -0600

    add session and security tokens

--- tests/unit/test_model.py
+++ tests/unit/test_model.py
@@ -100,6 +100,8 @@ def mock_environ():
     os.environ['AWS_DEFAULT_REGION'] = 'us-east-1'
     os.environ['AWS_ACCESS_KEY_ID'] = 'testing'
     os.environ['AWS_SECRET_ACCESS_KEY'] = 'testing'
+    os.environ['AWS_SECURITY_TOKEN'] = 'testing'
+    os.environ['AWS_SESSION_TOKEN'] = 'testing'
     os.environ['DYNAMODB_TABLE'] = 'c-132-table'
 
 
